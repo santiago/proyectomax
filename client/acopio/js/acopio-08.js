693920e9269bec7898da6a82000cafa72e615a82
Santiago GL <sgaviria@gmail.com>
2016-02-16 16:45:07 -0500

    more changes

---
+++ client/acopio/js/acopio-08.js
@@ -0,0 +1,11 @@
+$(function() {
+  $("td.aceptar a").click(function(e) {
+    e.preventDefault();
+    location.href = "Acopio 9.html";
+  });
+
+  $("td.cancelar a").click(function(e) {
+    e.preventDefault();
+    location.href = "Acopio 11.html";
+  });
+});
