5aca2d03c7f4b76b73dfc0f1d467d128e7bd6652
Santiago GL <sgaviria@gmail.com>
2016-02-17 00:44:44 -0500

    almost done

--- client/acopio/js/timer.js
+++ client/acopio/js/timer.js
@@ -2,6 +2,7 @@ $(function() {
   setInterval(function() {
     $.get("/recogidas", function(res, status, xhr) {
       res.forEach(function(i) {
+        if(!i) { return; }
         if(!localStorage.getItem("solicitud-"+i.id)) {
           localStorage.setItem("solicitud-"+i.id, true);
           localStorage.setItem("solicitud-seleccionada-id", i.id);
